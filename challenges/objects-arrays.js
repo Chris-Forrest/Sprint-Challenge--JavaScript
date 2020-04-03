c10a4aae7e0d38577748ae7c5463c45844095a70
Chris Forrest <redrider_cr@yahoo.com>
2020-04-02 21:30:54 -0400

    converted objects and arrays to arrow functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,10 +80,10 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, log the result. */
-const universities = [];
- graduates.forEach(function(item){
-   universities.push(item.university);
-});
+const universities = graduates.map((item) => item.university);
+// graduates.forEach(function(item){
+//   universities.push(item.university);
+//});
 
 
 
@@ -96,18 +96,19 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 
-const contactInfo = [];
-graduates.map(function(item){
-    return contactInfo.push(`${item.first_name} ${item.email}`);
-  });
+const contactInfo = graduates.map((item) => `${item.first_name} ${item.email}`);
+//graduates.map(function(item){
+  //  return contactInfo.push(`${item.first_name} ${item.email}`);
+ // });
   
 
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.filter(function(element){
-  return element.university.includes('Uni');
-});
+const unisWithUni = graduates.filter((element) => element.university.includes('Uni'));
+//graduates.filter(function(element){
+ // return element.university.includes('Uni');
+//});
 //const unisWithUni = 
 
 console.log(unisWithUni);
@@ -136,9 +137,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-zooAnimals.forEach(function(item){
-  return displayNames.push(`${item.animal_name} ${item.scientific_name}`);
-});
+zooAnimals.forEach((item) => displayNames.push `${item.animal_name} ${item.scientific_name}`);
+//zooAnimals.forEach(function(item){
+ // return displayNames.push(`${item.animal_name} ${item.scientific_name}`);
+//});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -148,9 +150,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
-zooAnimals.map(function(item){
-  return lowCaseAnimalNames.push(`${item.animal_name.toLowerCase()}`);
-});
+zooAnimals.map((item) => lowCaseAnimalNames.push `${item.animal_name.toLowerCase()}`);
+//zooAnimals.map(function(item){
+ // return lowCaseAnimalNames.push(`${item.animal_name.toLowerCase()}`);
+//});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -158,10 +161,10 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = 
-zooAnimals.filter(function(element){
-  return element.population < 5;
-});
+const lowPopulationAnimals = zooAnimals.filter((item) => item.population < 5);
+//zooAnimals.filter(function(element){
+  //return element.population < 5;
+//});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -169,10 +172,12 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 
-zooAnimals.reduce(function(accumulator, item){
-  return accumulator + item.population;
+const populationTotal = zooAnimals.reduce((accumulator, item) => {
+  return accumulator + item.population
 },0);
+//zooAnimals.reduce(function(accumulator, item){
+ // return accumulator + item.population;
+//},0);
 console.log(populationTotal);
 
 
